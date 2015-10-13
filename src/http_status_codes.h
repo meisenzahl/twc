41119f4dac945a484d8ef2e6ae19b7764a781c8a
Marius Meisenzahl <mariusmeisenzahl@gmail.com>
2015-10-13 16:46:08 +0200

    Added 404 status code

--- src/http_status_codes.h
+++ src/http_status_codes.h
@@ -4,4 +4,6 @@
 #define HTTP_STATUS_CODE_200_NO_CONTENT "HTTP/1.0 200 OK\r\nConnection: close\r\nContent-Length: 0\r\n\r\n"
 #define HTTP_STATUS_CODE_200 "HTTP/1.0 200 OK\r\nConnection: close\r\n"
 
+#define HTTP_STATUS_CODE_404_NO_CONTENT "HTTP/1.0 404 Not found\r\nConnection: close\r\nContent-Length: 0\r\n\r\n"
+
 #endif
